86893afe69d7fe84e71cd30e22df903ca80818ea
mirelasc23 <mirelamsccomach@gmail.com>
2025-12-11 20:45:06 -0300

    atualiza produtoCopaDAO, quartoDAO, receberDAO, reservaDAO, reservaQuartoDAO, servicoDAO, vagaEstacionamentoDAO e veiculoDAO

--- src/main/java/model/dao/VagaEstacionamentoDAO.java
+++ src/main/java/model/dao/VagaEstacionamentoDAO.java
@@ -2,32 +2,87 @@ package model.dao;
 
 import java.util.List;
 import model.VagaEstacionamento;
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 
 public class VagaEstacionamentoDAO implements InterfaceDAO<VagaEstacionamento>{
-
+private static VagaEstacionamentoDAO INSTANCE;
+    protected EntityManager entityManager;
+    
+    public VagaEstacionamentoDAO(){
+        entityManager = getEntityManager();
+    }
+    
+    public static VagaEstacionamentoDAO getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new VagaEstacionamentoDAO();
+        }
+        return INSTANCE;
+    }
+    
+    private EntityManager getEntityManager(){
+            EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU");
+            if(this.entityManager == null){
+                this.entityManager = factory.createEntityManager();
+            }
+            return this.entityManager;
+    }
+    
     @Override
     public void create(VagaEstacionamento objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            this.entityManager.getTransaction().begin();
+            this.entityManager.persist(objeto);
+            this.entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.entityManager.getTransaction().rollback();
+        }
     }
 
     @Override
     public VagaEstacionamento retrieve(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        VagaEstacionamento modelo = new VagaEstacionamento();
+        modelo = entityManager.find(VagaEstacionamento.class, id);
+        return modelo;
     }
 
     @Override
     public List<VagaEstacionamento> retrieve(String atributo, String valor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<VagaEstacionamento> modelos = new ArrayList<>();
+        modelos = entityManager.createQuery(" Select mar From modelo mar "
+                + " where " + atributo + " like (%" + valor + "%)",VagaEstacionamento.class).getResultList();
+        return modelos;
     }
 
     @Override
     public void update(VagaEstacionamento objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(objeto);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
     }
 
     @Override
     public void delete(VagaEstacionamento objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            entityManager.getTransaction().begin();
+            VagaEstacionamento modelo = new VagaEstacionamento();
+            modelo = entityManager.find(VagaEstacionamento.class, objeto.getId());
+            if(modelo != null){
+                entityManager.remove(modelo);
+            }
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
     }
-    
 }
--- src/main/java/model/dao/VeiculoDAO.java
+++ src/main/java/model/dao/VeiculoDAO.java
@@ -2,32 +2,87 @@ package model.dao;
 
 import java.util.List;
 import model.Veiculo;
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 
 public class VeiculoDAO implements InterfaceDAO<Veiculo>{
-
+private static VeiculoDAO INSTANCE;
+    protected EntityManager entityManager;
+    
+    public VeiculoDAO(){
+        entityManager = getEntityManager();
+    }
+    
+    public static VeiculoDAO getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new VeiculoDAO();
+        }
+        return INSTANCE;
+    }
+    
+    private EntityManager getEntityManager(){
+            EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU");
+            if(this.entityManager == null){
+                this.entityManager = factory.createEntityManager();
+            }
+            return this.entityManager;
+    }
+    
     @Override
     public void create(Veiculo objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            this.entityManager.getTransaction().begin();
+            this.entityManager.persist(objeto);
+            this.entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.entityManager.getTransaction().rollback();
+        }
     }
 
     @Override
     public Veiculo retrieve(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Veiculo modelo = new Veiculo();
+        modelo = entityManager.find(Veiculo.class, id);
+        return modelo;
     }
 
     @Override
     public List<Veiculo> retrieve(String atributo, String valor) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Veiculo> modelos = new ArrayList<>();
+        modelos = entityManager.createQuery(" Select mar From modelo mar "
+                + " where " + atributo + " like (%" + valor + "%)",Veiculo.class).getResultList();
+        return modelos;
     }
 
     @Override
     public void update(Veiculo objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(objeto);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
     }
 
     @Override
     public void delete(Veiculo objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            entityManager.getTransaction().begin();
+            Veiculo modelo = new Veiculo();
+            modelo = entityManager.find(Veiculo.class, objeto.getId());
+            if(modelo != null){
+                entityManager.remove(modelo);
+            }
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
     }
-    
 }
